700cacedd8fd6b320235b1223503d0a3705eb2fa
Samantha <Sambollano54@gmail.com>
2021-04-25 21:18:31 -0500

    .

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const Engineer = require ("./lib/Engineer.js");
 const Intern = require ("./lib/Intern.js");
 const Manager = require ("./lib/Manager.js");
 const Inquirer = require ('./lib/Inquirer');
-const fs = require ("fs");
+const Fs = require ("fs");
+const path = require ("path");
+const { reject } = require("lodash");
+const { reset } = require("ansi-styles");
 
 let employeeArr = [];
 
 const Questions = () => {
+    return new Promise((reset, reject) => {
     Inquirer.prompt([
         {
-            Type: 'List',
-            Message: 'EmployeeStatus',
-            Name: 'EmployeeName',
-            Choices: [Employee, Engineer, Manager, Intern]
+            type: 'List',
+            message: 'EmployeeStatus',
+            name: 'EmployeeName',
+            choices: [Employee, Engineer, Manager, Intern]
         },
+        {
+            type: 'list',
+            message: 'EmployeeId',
+            name: 'Id',
+        }
+        
     ])
 }
