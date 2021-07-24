23c8afddc752cbbef1cf542aca06208c0bb9bd83
Samantha Arellano <Sambollano54@gmail.com>
2021-07-23 21:09:28 -0500

    update

--- app.js
+++ app.js
@@ -11,56 +11,57 @@ const {
     EngineerQuestions,
     InternQuestions
 } = require("./lib/Inquire");
+const inquire = require("./lib/Questions.js");
+const Employee = require("./lib/Employee.js");
+const { managerQuestions, defineEmployee, internQuestions, engineerQuestions } = require("./lib/Questions.js");
 
 
 function init() {
-    startHtml();
-    addMembers();
-}
-
-  function addMembers() {
-      inquirer.prompt([{
-          type: "input",
-          message: "Team Members Role",
-          choices: [
-              "Manager",
-              "Intern",
-              "Engineer"
-          ],
-          name: "role"
-        },
-        {
-          message: "EmployeeId",
-          name: "Id",
-        },
-        {
-          message: "EmployeeEmail",
-          name: "Email",
-        },
-       ]);
-
-    // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-
-    const generatedFile = game.js(response)  
-     
-     console.log(generatedFile);
-    };
-    function writeToFile(fileName, data) {
-        fs.writeFile(fileName, data, err => {
-            if (err)
-                throw new Error(err);
-            console.log("Readme is successfully created")
+    inquire
+    .prompt(Empoloyeesquestions.contcat(managerQuestions))
+    .then(({name, id, email, officeNumber}) => {
+        let manager = new Manager(name, id, email, officeNumber);
+        employeeArray.push(manager);
+        getEmployee();
     })
 }
 
-// TODO: Create a function to initialize app
-function init() {
-        inquirer.prompt(questions)
-        .then((inquirerResponse) => {   
-            console.log("Generated");
+  function getEmployee() {
+      inquirer
+      .prompt(defineEmployee)
+      .then(({userChoice}) => {
+          switch (userChoice) {
+            case 'intern':
+            getIntern();
+            break;
+            case 'Complete':
+            htmlRenderer(employeeArray);
+            break;
+            case 'Engineer':
+            getEngineer();
+            break;
+          }
+      })
+    }
+
+    function getIntern() {
+        inquire
+        .prompt(Empoloyeesquestions.concat(internQuestions))
+        .then(({name, id, email, school}) => {
+            let intern = new Intern(name, id, email, school);
+            employeeArray.push(intern)
+            getEmployee();
+        }) 
+    }
+
+    function getEngineer() {
+        inquire
+        .prompt(employeeQuestions.concat(engineerQuestions))
+        .then(({name, id, email, githubUsername}) => {
+            let engineer = new Engineer(name, id, email, githubUsername);
+            employeeArray.push(engineer);
+            getEmployee();
         })
-        .catch((err) => {
-            console.log(err);
-        })
-      }}
+    }
+
+    init();
